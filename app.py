9e70d1bd322e70f5750d5d27400c6cdb0dc6b7a0
chirayu gupta <chirayugupta14@gmail.com>
2024-10-31 13:28:17 -0400

    added registration functionality

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash, session
 from tools import Tool
 from reviews import Review
 from users import User
 from messages import Message
 
 app = Flask(__name__)
+app.secret_key = '2bshd9ei3nd40fk'
 
 @app.route("/")
 def index():
@@ -32,3 +33,27 @@ def add_tool():
         last_id = tools[-1]['id']
         
         return render_template('market.html')
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if request.method == 'GET':
+        return render_template('registration.html')
+    elif request.method == 'POST':
+        users = User.getAllUsers()
+        last_id = int(users[-1]['user_id'])
+        user_id = last_id + 1
+        username = request.form['username']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        password = request.form['password']
+        profile_image = request.form['profile_picture']
+        location = request.form['location']
+
+        if not username or not password or not fname or not lname or not email or not location:
+            flash('All required fields must be filled out.', 'error')
+        else:
+            name = str(fname) + str(lname)
+            User.addUser(user_id, username, name, email, password, profile_image, location)
+            return redirect(url_for('index'))
+    
--- users.py
+++ users.py
@@ -1,8 +1,9 @@
 import sqlite3
 
 class User:
-    def __init__(self, user_id, name, email, password, profile_image, location):
+    def __init__(self, user_id, username, name, email, password, profile_image, location):
         self.__userID = user_id
+        self.__username = username
         self.__name = name
         self.__email = email
         self.__password = password
@@ -49,8 +50,16 @@ class User:
     def location(self, value):
         self.__location = value
 
+    @property
+    def username(self):
+        return self.__username
+    
+    @username.setter
+    def username(self,value):
+        self.__username = value
+
     def __str__(self):
-        return "Resource Title: {} \n Description: {} \n Category: {} \n Availability: {} \n Date Posted: {}".format(self.title, self.description,self.category,self.availability,self.datePosted)
+        return "Username: {} \n Name: {} \n Email: {} \n Location: {} ".format(self.username, self.name,self.email,self.location)
     
     @classmethod
     def delUser(cls, user_id):
@@ -63,23 +72,23 @@ class User:
         conn.close()
 
     @classmethod
-    def addUser(cls, user_id, name, email, password, profile_image, location):
+    def addUser(cls, user_id, username, name, email, password, profile_image, location):
         conn = None
         conn = sqlite3.connect( "database.db")
-        sql='INSERT INTO users ( user_id, name, email, password, profile_image, location) values (?,?,?,?,?,?)'
+        sql='INSERT INTO users ( user_id,username, name, email, password, profile_image, location) values (?,?,?,?,?,?,?)'
         cur = conn.cursor()
-        cur.execute(sql, ( user_id, name, email, password, profile_image, location ))
+        cur.execute(sql, ( user_id, username,name, email, password, profile_image, location,))
         conn.commit()
         if conn:
             conn.close()
 
     @classmethod
-    def updateUser(cls,  user_id, name, email, password, profile_image, location):
+    def updateUser(cls,  user_id, username, name, email, password, profile_image, location):
         conn = None
         conn = sqlite3.connect('database.db')
-        sql='UPDATE users SET name=?, email=?, password=?, profile_image=?, location = ? WHERE user_id = ?'
+        sql='UPDATE users SET username=?, name=?, email=?, password=?, profile_image=?, location = ? WHERE user_id = ?'
         cur = conn.cursor()
-        cur.execute(sql, (name, email, password, profile_image, location, user_id))
+        cur.execute(sql, (username, name, email, password, profile_image, location, user_id,))
         conn.commit()
         conn.close()
 
@@ -87,11 +96,11 @@ class User:
     def getAllUsers(cls):
         conn = sqlite3.connect('database.db')
         cursorObj = conn.cursor()
-        cursorObj.execute('SELECT user_id, name, email, password, profile_image, location FROM users;')
+        cursorObj.execute('SELECT user_id, username, name, email, password, profile_image, location FROM users;')
         allRows = cursorObj.fetchall()
         ListOfDictionaries = []
         for row in allRows:
-            m = {"user_id": row[0], "name":row[1], 'email': row[2], 'password':row[3], 'profile_image':row[4], 'location':row[5]}
+            m = {"user_id": row[0], 'username':row[1],"name":row[2], 'email': row[3], 'password':row[4], 'profile_image':row[5], 'location':row[6]}
             ListOfDictionaries.append(m)
         if conn:
             conn.close()
@@ -103,16 +112,16 @@ def InsertStartingData():
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
     user_data = [
-        (1, 'John Doe', 'johndoe@example.com', 'hashed_password_1', '/images/johndoe.jpg', 'New York, NY'),
-        (2, 'Jane Smith', 'janesmith@example.com', 'hashed_password_2', '/images/janesmith.jpg', 'Los Angeles, CA')]
-    cur.executemany('''INSERT INTO Users (user_id, name, email, password, profile_image, location) VALUES (?, ?, ?, ?, ?, ?)''', user_data)
+        (1, 'jdoe123','John Doe', 'johndoe@example.com', 'hashed_password_1', '/images/johndoe.jpg', 'New York, NY'),
+        (2, 'jsmith2030','Jane Smith', 'janesmith@example.com', 'hashed_password_2', '/images/janesmith.jpg', 'Los Angeles, CA')]
+    cur.executemany('''INSERT INTO Users (user_id, username, name, email, password, profile_image, location) VALUES (?, ?, ?, ?, ?, ?, ?)''', user_data)
     conn.commit()
     conn.close()
 
 def createUserTable():
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
-    cur.execute('''CREATE TABLE Users (user_id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,email TEXT NOT NULL UNIQUE,password TEXT NOT NULL,profile_image TEXT,location TEXT);''')
+    cur.execute('''CREATE TABLE Users (user_id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT NOT NULL, name TEXT NOT NULL,email TEXT NOT NULL UNIQUE,password TEXT NOT NULL,profile_image TEXT,location TEXT);''')
     conn.commit()
     conn.close()
 
